93cbe7722b349f1df822aa434777154043f7f7d1
Angel Flores <floresangel.dev@gmail.com>
2025-05-02 19:59:27 -0300

    first commit: uploading frontend

---
+++ js/products.js
@@ -0,0 +1,183 @@
+// Variable global para la URL base de la API
+const API_BASE_URL = "https://tapiceriafast-backend.onrender.com";
+
+// Variable global para almacenar todos los productos
+let allProducts = [];
+
+// Función para filtrar productos
+function filterProducts() {
+    const searchValue = document.getElementById('searchInput').value.toLowerCase();
+    const products = document.getElementsByClassName('product');
+    
+    Array.from(products).forEach(product => {
+        const title = product.querySelector('h4').textContent.toLowerCase();
+        title.includes(searchValue) 
+            ? product.classList.remove('hidden')
+            : product.classList.add('hidden');
+    });
+}
+
+// Función para abrir el modal con la información completa del producto
+function openProductModal(product) {
+    const modal = document.getElementById('productModal');
+    const modalContent = modal.querySelector('.modal-content');
+
+    // Limpiar contenido previo
+    modalContent.querySelector('.modal-slider').innerHTML = '';
+    modalContent.querySelector('.modal-title').textContent = '';
+    modalContent.querySelector('.modal-description').textContent = '';
+    modalContent.querySelector('.modal-color-buttons').innerHTML = '';
+    modalContent.querySelector('.modal-price').textContent = '';
+
+    // Llenar el modal con la información del producto
+    modalContent.querySelector('.modal-title').textContent = product.title;
+
+    if (product.description) {
+        modalContent.querySelector('.modal-description').textContent = 
+            product.description[0]?.children[0]?.text || '';
+    }
+
+    if (product.price) {
+        modalContent.querySelector('.modal-price').innerHTML = `<sup>$</sup>${product.price}`;
+    }
+
+    // Slider de imágenes
+    const sliderContainer = modalContent.querySelector('.modal-slider');
+    const sliderImages = document.createElement('div');
+    sliderImages.className = 'slider-images';
+    sliderContainer.appendChild(sliderImages);
+
+    // Botones de color
+    const colorButtons = modalContent.querySelector('.modal-color-buttons')
+
+    let activeImageUrl = `${product.colorImage[0].image.url}`;
+
+    product.colorImage.forEach((colorImage, index) => {
+        const img = document.createElement('img');
+        img.src = `${colorImage.image.url}`;
+        img.alt = `${product.title} - Opción ${index + 1}`;
+        img.className = `slider-image${index === 0 ? ' active' : ''}`;
+        img.dataset.fancybox = `gallery-${product.id}`;
+        sliderImages.appendChild(img);
+
+        const colorBtn = document.createElement('button');
+        colorBtn.className = `color-button${index === 0 ? ' active' : ''}`;
+
+        const colors = colorImage.colorPicker.map(c => c.color);
+        if (colors.length === 1) {
+            colorBtn.style.backgroundColor = colors[0];
+        } else {
+            colorBtn.style.background = `
+                linear-gradient(135deg, 
+                    ${colors.map((c, i) => 
+                        `${c} ${(i * 100)/colors.length}% ${((i + 1) * 100)/colors.length}%`
+                    ).join(', ')}
+                )`;
+        }
+
+        colorBtn.addEventListener('click', () => {
+            sliderImages.querySelectorAll('.slider-image').forEach(img => img.classList.remove('active'));
+            colorButtons.querySelectorAll('.color-button').forEach(btn => btn.classList.remove('active'));
+
+            img.classList.add('active');
+            colorBtn.classList.add('active');
+            sliderImages.style.transform = `translateX(-${index * 100}%)`;
+
+            activeImageUrl = `${colorImage.image.url}`;
+
+            const ctaButton = modalContent.querySelector('#cta-button-modal');
+            const whatsappMessage = `Hola, quería consultar por el producto: ${product.title}. ${activeImageUrl}`;
+            const whatsappLink = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(whatsappMessage)}`;
+            ctaButton.href = whatsappLink;
+        });
+
+        colorButtons.appendChild(colorBtn);
+    });
+
+    const ctaButton = modalContent.querySelector('#cta-button-modal');
+    const whatsappMessage = `Hola, quería consultar por el producto: ${product.title}. ${activeImageUrl}`;
+    const whatsappLink = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(whatsappMessage)}`;
+    ctaButton.href = whatsappLink;
+
+    // Mostrar el modal
+    modal.classList.add('show'); // Activar la animación
+
+    // Body no scroll
+    // disableScroll();
+
+
+    // Cerrar modal al hacer clic en la X
+    modal.querySelector('.close-modal').onclick = () => {
+        modal.classList.remove('show'); // Eliminar la animación cuando se cierra el modal
+        // document.body.classList.remove('no-scroll');
+        // enableScroll();
+    };
+
+    // Cerrar modal al hacer clic fuera del contenido
+    window.onclick = (event) => {
+        if (event.target === modal) {
+            modal.classList.remove('show'); // Eliminar la animación cuando se cierra el modal
+            // document.body.classList.remove('no-scroll');
+            // enableScroll();
+        }
+    };
+}
+
+
+// Función principal para cargar y mostrar productos
+function loadProducts() {
+    fetch(`${API_BASE_URL}/api/productos?fields[0]=title&fields[1]=description&fields[2]=price&populate[colorImage][populate][image][fields][0]=name&populate[colorImage][populate][image][fields][1]=url&populate[colorImage][populate][colorPicker][fields][0]=color`)
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('productsContainer');
+            allProducts = data.data; // Almacenar todos los productos
+
+            data.data.forEach(product => {
+                // Crear contenedor del producto
+                const productDiv = document.createElement('div');
+                productDiv.className = 'product';
+                productDiv.innerHTML = `
+                            <div class="slider">
+                                <div class="slider-images">
+                                    ${product.colorImage.map((colorImage, index) => `
+                                        <img src="${colorImage.image.url}"
+                                            class="slider-image${index === 0 ? ' active' : ''}"
+                                            alt="${product.title} - Variante ${index + 1}"
+                                            ${index !== 0 ? 'loading="lazy"' : ''}>>
+                                    `).join('')}
+                                </div>
+                            </div>
+                            <p>$${product.price}</p>
+                            <h4>${product.title}</h4>
+                `;
+
+                // Agregar evento de clic para abrir el modal
+                productDiv.addEventListener('click', () => openProductModal(product));
+                container.appendChild(productDiv);
+            });
+
+            // Inicializar Fancybox
+            Fancybox.bind('[data-fancybox]', {
+                Thumbs: { type: 'classic' },
+                Toolbar: {
+                    display: {
+                        left: ['infobar'],
+                        middle: ['zoomIn', 'zoomOut', 'toggle1to1'],
+                        right: ['slideshow', 'thumbs', 'close'],
+                    },
+                },
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+// Cargar productos cuando la página esté lista
+document.addEventListener('DOMContentLoaded', () => {
+    loadProducts();
+    // lazyLoadProducts();
+    
+    // Configurar el evento de búsqueda
+    const searchInput = document.getElementById('searchInput');
+    searchInput.addEventListener('input', filterProducts);
+});
---
+++ js/script.js
@@ -0,0 +1,121 @@
+// Variable global para el número de WhatsApp
+const WHATSAPP_NUMBER = "3854031564";
+
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".nav");
+
+navToggler.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  navToggler.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  navToggler.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// toggleTheme
+const toggleDos = document.getElementById("checkbox-input");
+const logo = document.querySelector("#logo");
+
+// Verificar el modo al cargar la página
+const darkModeEnabled = localStorage.getItem("dark-mode") === "true";
+toggleDos.checked = darkModeEnabled;
+document.body.classList.toggle("dark-mode", darkModeEnabled);
+// logo.src = darkModeEnabled ? "images/logo-claro.png" : "images/logo-oscuro.png";
+
+toggleDos.addEventListener("change", function () {
+  const isChecked = this.checked;
+  document.body.classList.toggle("dark-mode", isChecked);
+  localStorage.setItem("dark-mode", isChecked);
+
+  // Cambiar la imagen según el modo
+  logo.src = isChecked ? "images/logo-claro.png" : "images/logo-oscuro.png";
+});
+
+// Obtener el botón
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Mostrar el botón cuando se desplaza hacia abajo
+window.onscroll = function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Función para subir a la parte superior
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// Enviar consulta personalizada
+document
+  .getElementById("ctaMessage")
+  .addEventListener("click", function (event) {
+    // Prevenir el comportamiento predeterminado del enlace
+    event.preventDefault();
+
+    // Obtener el valor del textarea, eliminamos espacios en blanco a los extremos
+    const textarea = document.getElementById("textarea").value.trim();
+
+    // Validar que el textarea no esté vacío
+    if (textarea === "") {
+      alert("Por favor, escribe tu consulta antes de enviar.");
+      return;
+    }
+
+    // Crear el enlace de WhatsApp con el mensaje personalizado
+    const msgPersonalizado = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(
+      textarea
+    )}`;
+
+    // Asignar el enlace al href del botón
+    this.href = msgPersonalizado;
+
+    // Abrir el enlace en una nueva pestaña
+    window.open(msgPersonalizado, "_blank");
+  });
+
+// Crear un observer para detectar cuando la sección entra en el viewport
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Aquí puedes cargar el contenido o mostrar la sección
+      const section = entry.target;
+      section.classList.add("cargada");
+      // Puedes hacer que se cargue contenido dinámicamente si es necesario
+      observer.unobserve(section);
+    }
+  });
+});
+
+// detectad scroll
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+
+  // Verifica si el desplazamiento vertical es mayor que 0
+  if (window.scrollY > 5) {
+    header.classList.add("scrolled"); // Agrega la clase 'scrolled'
+  } else {
+    header.classList.remove("scrolled"); // Remueve la clase 'scrolled'
+  }
+});
+
+// Selecciona las secciones que quieres cargar de manera diferida
+const lazySections = document.querySelectorAll(".contenido-lento");
+// Observa cada sección
+lazySections.forEach((section) => observer.observe(section));
+
+
+const contactBtn = document.getElementById("contactBtnMain");
+contactBtn.href = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent('Hola, me gustaría realizar una consulta.')}`
